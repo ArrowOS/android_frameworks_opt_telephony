72fc3dd145f2806eb3d15668fba73d6eac26ac67
Sneh Bansal <snehb@codeaurora.org>
2018-02-05 12:14:06 +0530

    Fix wrong APN list issue for multimode SIM cards.
    For multimode cards with CSIM, USIM apps, if RUIMRecords are loaded
    first, then APNs will be read with operator numeric from RUIMRecords,
    leading to data call issues with wrong APNs.
    
    Add fix to wait for SIMRecords loaded event to create APN list.
    
    Change-Id: Ibb90188dfbb8270844703a7a271e260bae171755
    CRs-Fixed: 2178427

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -2894,6 +2894,11 @@ public class GsmCdmaPhone extends Phone {
         }
     }
 
+    @Override
+    public SIMRecords getSIMRecords() {
+        return mSimRecords;
+    }
+
     private void processIccRecordEvents(int eventCode) {
         switch (eventCode) {
             case IccRecords.EVENT_CFI:
