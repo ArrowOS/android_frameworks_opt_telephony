93e28e4df688fa1e4b197ad356956d2ac1328ed6
Umashankar Godachi <umasha@codeaurora.org>
2018-05-15 15:21:27 +0530

    MSIM: Fix to refresh active subscription info cache.
    During hotswap, active subscription info cache needs
    to be refreshed, but currently refresh is happening even
    before slotId to subId mapping is done resulting in stale
    data getting retrieved.
    
    Fix: Refresh active subscription info cache only after slotId
         to subId mapping is done.
    
    Change-Id: Ie06c804e28af16a9cfb3c47fd4d32c5f5968930c
    CRs-Fixed: 2228217
    Signed-off-by: xyyx <xyyx@mail.ru>

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -971,9 +971,6 @@ public class SubscriptionController extends ISub.Stub {
                         resolver.update(SubscriptionManager.CONTENT_URI, value,
                                 SubscriptionManager.UNIQUE_KEY_SUBSCRIPTION_ID +
                                         "=" + Long.toString(subId), null);
-
-                        // Refresh the Cache of Active Subscription Info List
-                        refreshCachedActiveSubscriptionInfoList();
                     }
 
                     if (DBG) logdl("[addSubInfoRecord] Record already exists");
@@ -1067,11 +1064,11 @@ public class SubscriptionController extends ISub.Stub {
                         SubscriptionManager.UNIQUE_KEY_SUBSCRIPTION_ID +
                                 "=" + Long.toString(subId), null);
 
-                // Refresh the Cache of Active Subscription Info List
-                refreshCachedActiveSubscriptionInfoList();
-
                 if (DBG) logdl("[addSubInfoRecord] sim name = " + nameToSet);
             }
+
+            // Refresh the Cache of Active Subscription Info List
+            refreshCachedActiveSubscriptionInfoList();
 
             // Once the records are loaded, notify DcTracker
             sPhones[slotIndex].updateDataConnectionTracker();
@@ -1115,9 +1112,6 @@ public class SubscriptionController extends ISub.Stub {
         }
 
         Uri uri = resolver.insert(SubscriptionManager.CONTENT_URI, value);
-
-        // Refresh the Cache of Active Subscription Info List
-        refreshCachedActiveSubscriptionInfoList();
 
         return uri;
     }
