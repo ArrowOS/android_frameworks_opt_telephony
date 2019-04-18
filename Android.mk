8dd02babc6f35d4e9f5046f9c33e12c91d6297bd
Bevis Tseng <bevis@cloudmosa.com>
2019-04-18 16:30:45 +0800

    Reverse-engineer MTK IRadio interfaces to approve incoming call for MTK devices from Telephony F/W.
    This is an alternative solution of 's/AT+EAIC=2/AT+EAIC=3/g' in device/phh/treble/rw-system.sh
    which is not workable on some MTK devices such as Nokia 1.
    Make sure that the solution in `rw-system.sh` is reversed if this alternative is applied.
    
    Here is the reason of each API to be reverse-engineered in this patch:
    1. IRadio::setResponseFunctionsMtk():
       - To register callbacks of MTK-extended radio indications and responses.
    2. IRadioIndication::incomingCallIndication():
       - To receive the indication of the incoming call to be approved.
    3. IRadio::setCallIndication() |
       IRadioResponse::setCallIndicationResponse():
       - To approve each the incoming call reported from incomingCallIndication().
    4. IRadioResponse::setupDataCallResponseEx() |
       IRadioResponse::getDataCallListResponseEx() |
       IRadioIndication::dataCallListChangedEx():
       - These callbacks are MTK's replacements of the following AOSP callbacks:
         setupDataCallResponse(), getDataCallListResponse(), dataCallListChanged() if
         MTK-specific implementation is registered via IRadio::setResponseFunctionsMtk().
       - To make sure that data connections are workable after fixing the incoming call issues,
         we have to implement these methods additionally to forward the responses to the replaced ones defined by AOSP.
    
    Change-Id: I0fb1a0dfb45ef4790e794dc8546be702c49f6799

--- Android.mk
+++ Android.mk
@@ -35,6 +35,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android.hidl.base-V1.0-java \
     ims-ext-common
 
+LOCAL_STATIC_JAVA_LIBRARIES += vendor.mediatek.hardware.radio-V2.0-java
+
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := telephony-common
 
