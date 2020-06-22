191f9bc51b76685a93a389e5e5af4614b26cbefe
Susheel Nyamala <snyamala@codeaurora.org>
2020-06-22 15:57:44 +0530

    Telcel: Fix data call retry for apn change event
    Whenever apn is edited, cleanup is triggered and retry is
    scheduled. This retry cancels the retry scheduled for Telcel
    feature affecting the data call retry pattern
    
    Fix is to skip retry for apn change
    
    Change-Id: I65ce13983afd06622bb8feefa0828cac433618f6
    CRs-Fixed: 2711425

--- src/java/com/android/internal/telephony/vendor/dataconnection/VendorDcTracker.java
+++ src/java/com/android/internal/telephony/vendor/dataconnection/VendorDcTracker.java
@@ -19,13 +19,16 @@ package com.android.internal.telephony.vendor.dataconnection;
 import android.app.AlertDialog;
 import android.telephony.AccessNetworkConstants;
 import android.telephony.DataFailCause;
+import android.telephony.data.ApnSetting;
 import android.telephony.Rlog;
 import android.telephony.ServiceState;
 import android.telephony.SubscriptionManager;
+import android.text.TextUtils;
 import android.view.WindowManager;
 
 import com.android.internal.telephony.DctConstants;
 import com.android.internal.telephony.Phone;
+import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.dataconnection.ApnContext;
 import com.android.internal.telephony.dataconnection.DcTracker;
 
@@ -123,10 +126,42 @@ public class VendorDcTracker extends DcTracker {
             RetryFailures retryFailures) {
         if (mPhone.getContext().getResources().getBoolean(
                 com.android.internal.R.bool.config_pdp_reject_enable_retry) &&
-                mDataRejectCount > 0) {
+                mDataRejectCount > 0 &&
+                TextUtils.equals(apnContext.getApnType(), PhoneConstants.APN_TYPE_DEFAULT)) {
             log("setupDataOnConnectableApn: data retry in progress, skip processing");
         } else {
             super.setupDataOnConnectableApn(apnContext, reason, retryFailures);
+        }
+    }
+
+    @Override
+    protected void cleanUpConnectionInternal(boolean detach, @ReleaseNetworkType int releaseType,
+            ApnContext apnContext) {
+        if (apnContext == null) {
+            if (DBG) log("cleanUpConnectionInternal: apn context is null");
+            return;
+        }
+
+        if (mPhone.getContext().getResources().getBoolean(
+                 com.android.internal.R.bool.config_pdp_reject_enable_retry) &&
+                 mDataRejectCount > 0 &&
+                 TextUtils.equals(apnContext.getApnType(), PhoneConstants.APN_TYPE_DEFAULT)) {
+            log("cleanUpConnectionInternal: data retry in progress, skip cleanup");
+        } else {
+            super.cleanUpConnectionInternal(detach, releaseType, apnContext);
+        }
+    }
+
+    @Override
+    protected boolean retryAfterDisconnected(ApnContext apnContext) {
+        if (mPhone.getContext().getResources().getBoolean(
+                com.android.internal.R.bool.config_pdp_reject_enable_retry) &&
+                mDataRejectCount > 0 &&
+                TextUtils.equals(apnContext.getApnType(), PhoneConstants.APN_TYPE_DEFAULT)) {
+            log("retryAfterDisconnected: data retry in progress, skip this retry");
+            return false;
+        } else {
+            return super.retryAfterDisconnected(apnContext);
         }
     }
 
@@ -136,7 +171,8 @@ public class VendorDcTracker extends DcTracker {
         boolean isPdpRejectConfigEnabled = mPhone.getContext().getResources().getBoolean(
                 com.android.internal.R.bool.config_pdp_reject_enable_retry);
         if (success) {
-            if (isPdpRejectConfigEnabled) {
+            if (isPdpRejectConfigEnabled &&
+                    TextUtils.equals(apnContext.getApnType(), PhoneConstants.APN_TYPE_DEFAULT)) {
                 handlePdpRejectCauseSuccess();
             }
         } else {
@@ -151,8 +187,15 @@ public class VendorDcTracker extends DcTracker {
             @RequestNetworkType int requestType) {
         long delay = apnContext.getDelayForNextApn(mFailFast);
         if (mPhone.getContext().getResources().getBoolean(
-                com.android.internal.R.bool.config_pdp_reject_enable_retry)) {
+                com.android.internal.R.bool.config_pdp_reject_enable_retry) &&
+                TextUtils.equals(apnContext.getApnType(), PhoneConstants.APN_TYPE_DEFAULT)) {
             String reason = DataFailCause.toString(mPdpRejectCauseCode);
+            //Reset apn permanent failure to allow retry
+            ApnSetting apn = apnContext.getApnSetting();
+            if (apn != null) {
+                if (DBG) log("onDataSetupCompleteError: reset permanent failure on apn");
+                apn.setPermanentFailed(false);
+            }
 
             if (isMatchingPdpRejectCause(reason)) {
                 if (mVendorDataResetEventTracker == null) {
